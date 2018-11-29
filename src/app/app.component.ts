752feb6b4aadd91b63ac2f265119d91d8f58c1ca
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-11-29 11:29:27 -0500

    Flushing out the UI more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator App';
+  num1: number;
+  num2: number;
+  result: number;
+
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+
+  subtract() {
+    this.result = this.num1 - this.num2;
+  }
+
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+
+  divide() {
+    this.result = this.num1 / this.num2;
+  }
+
+  remainder() {
+    this.result = this.num1 % this.num2;
+  }
 }
